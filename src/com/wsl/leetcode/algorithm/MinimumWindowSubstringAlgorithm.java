1c98aef746226aaa033a80c68ebd82b0925e98c7
wushaolin <wushaolin@kaolazhengxin.com>
2020-09-24 18:19:58 +0800

    最小覆盖子串

--- src/com/wsl/leetcode/algorithm/MinimumWindowSubstringAlgorithm.java
+++ src/com/wsl/leetcode/algorithm/MinimumWindowSubstringAlgorithm.java
@@ -1,5 +1,9 @@
 package com.wsl.leetcode.algorithm;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * 最小覆盖子串
  * 
@@ -17,8 +21,61 @@ package com.wsl.leetcode.algorithm;
  */
 public class MinimumWindowSubstringAlgorithm {
 
+    Map<Character, Integer> ori = new HashMap<>();
+
+    Map<Character, Integer> cnt = new HashMap<>();
+
+    /**
+     * 滑动窗口（该题没有解题思路，参考官网的答案，实现的原理暂时未能理解）
+     * 参考：https://leetcode-cn.com/problems/minimum-window-substring/solution/zui-xiao-fu-gai-zi-chuan-by-leetcode-solution
+     * 
+     * @param s
+     * @param t
+     * @return
+     */
     public String minWindow(String s, String t) {
-        return "";
+        for (int i = 0; i < t.length(); i++) {
+            ori.put(t.charAt(i), ori.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    private boolean check() {
+        Iterator iterator = ori.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry entry = (Map.Entry)iterator.next();
+            Character key = (Character)entry.getKey();
+            Integer value = (Integer)entry.getValue();
+            if (cnt.getOrDefault(key, 0) < value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        MinimumWindowSubstringAlgorithm minimumWindowSubstringAlgorithm = new MinimumWindowSubstringAlgorithm();
+        System.out.println(minimumWindowSubstringAlgorithm.minWindow("ADOBECODEBANC", "ABC"));
     }
 
 }
