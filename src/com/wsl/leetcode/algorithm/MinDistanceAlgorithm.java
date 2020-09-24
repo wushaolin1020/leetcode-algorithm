9eb93df31761b19c8abcef9b50de3a0da544805d
wushaolin <wushaolin@kaolazhengxin.com>
2020-09-24 14:56:52 +0800

    编辑距离算法，该题没思路

---
+++ src/com/wsl/leetcode/algorithm/MinDistanceAlgorithm.java
@@ -0,0 +1,87 @@
+package com.wsl.leetcode.algorithm;
+
+/**
+ * 编辑距离
+ *
+ * 给你两个单词word1和word2，请你计算出将word1转换成word2 所使用的最少操作数。
+ *
+ * 你可以对一个单词进行如下三种操作：
+ *
+ * 插入一个字符 删除一个字符 替换一个字符
+ *
+ * 示例1：
+ *
+ * 输入：word1 = "horse", word2 = "ros" 输出：3 解释： horse -> rorse (将 'h' 替换为 'r') rorse -> rose (删除 'r') rose -> ros (删除 'e')
+ *
+ * 示例2：
+ *
+ * 输入：word1 = "intention", word2 = "execution" 输出：5 解释： intention -> inention (删除 't') inention -> enention (将 'i' 替换为
+ * 'e') enention -> exention (将 'n' 替换为 'x') exention -> exection (将 'n' 替换为 'c') exection -> execution (插入 'u')
+ *
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/edit-distance 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class MinDistanceAlgorithm {
+
+    /**
+     * 动态规划（该题没有解题思路，参考官网的答案）
+     * 
+     * D[i][j-1] 为 A 的前 i 个字符和 B 的前 j - 1 个字符编辑距离的子问题。即对于 B 的第 j 个字符，我们在 A 的末尾添加了一个相同的字符，那么 D[i][j] 最小可以为 D[i][j-1] + 1；
+     *
+     * D[i-1][j] 为 A 的前 i - 1 个字符和 B 的前 j 个字符编辑距离的子问题。即对于 A 的第 i 个字符，我们在 B 的末尾添加了一个相同的字符，那么 D[i][j] 最小可以为 D[i-1][j] + 1；
+     *
+     * D[i-1][j-1] 为 A 前 i - 1 个字符和 B 的前 j - 1 个字符编辑距离的子问题。即对于 B 的第 j 个字符，我们修改 A 的第 i 个字符使它们相同，那么 D[i][j] 最小可以为
+     * D[i-1][j-1] + 1。特别地，如果 A 的第 i 个字符和 B 的第 j 个字符原本就相同，那么我们实际上不需要进行修改操作。在这种情况下，D[i][j] 最小可以为 D[i-1][j-1]。
+     *
+     * 若 A 和 B 的最后一个字母相同：D[i][j]=min(D[i][j−1]+1,D[i−1][j]+1,D[i−1][j−1])
+     *
+     * 若 A 和 B 的最后一个字母不同： D[i][j]=min(D[i][j−1]+1,D[i−1][j]+1,D[i−1][j−1]+1)
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/edit-distance/solution/bian-ji-ju-chi-by-leetcode-solution/ 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * 
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        // 当有字符串为空
+        if (m * n == 0) {
+            return m + n;
+        }
+
+        // dp数组
+        int[][] dp = new int[m + 1][n + 1];
+
+        // 初始化边界值
+        for (int i = 0; i < m + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < n + 1; j++) {
+            dp[0][j] = j;
+        }
+
+        // 计算所有dp值
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                int left = dp[i][j - 1] + 1;
+                int down = dp[i - 1][j] + 1;
+                int left_down = dp[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    left_down++;
+                }
+                dp[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        MinDistanceAlgorithm minDistanceAlgorithm = new MinDistanceAlgorithm();
+        System.out.println(minDistanceAlgorithm.minDistance("intention", "execution"));
+    }
+}
